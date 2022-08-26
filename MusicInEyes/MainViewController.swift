4e43ad30f9af46d6999f6fd911ccff4c9733df45
lena <lenaminn@gmail.com>
2022-08-26 14:24:46 +0900

    [FEAT] Record 버튼 생성

--- MusicInEyes/MainViewController.swift
+++ MusicInEyes/MainViewController.swift
@@ -9,4 +9,43 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    lazy var recordButton: UIButton = {
+        let buttom = UIButton()
+
+        buttom.backgroundColor = .customNavy
+        buttom.layer.cornerRadius = (UIScreen.main.bounds.width * 0.2) / 2
+        buttom.layer.masksToBounds = false
+        buttom.layer.shadowRadius = 7.0
+        buttom.layer.shadowOpacity = 0.2
+        buttom.layer.borderColor = UIColor.customNavy.cgColor
+        buttom.layer.borderWidth = 5.0
+        
+        buttom.setImage(UIImage(systemName: "headphones"), for: .normal)
+        buttom.tintColor = .white
+        buttom.contentMode = .scaleAspectFill
+        buttom.contentVerticalAlignment = .fill
+        buttom.contentHorizontalAlignment = .fill
+        
+        buttom.translatesAutoresizingMaskIntoConstraints = false
+        
+        return buttom
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Music in Eyes"
+        
+        view.backgroundColor = .white
+        [recordButton].forEach { view.addSubview($0) }
+        configureConstraints()
+        
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            recordButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.2),
+            recordButton.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 0.2),
+            recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            recordButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -UIScreen.main.bounds.height / 15)])
+    }
 }
