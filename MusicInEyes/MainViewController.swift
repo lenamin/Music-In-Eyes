ef420a26647176b3ceb3d2a6e80604c9fdd9a4cd
lena <lenaminn@gmail.com>
2022-08-26 18:13:09 +0900

    [FEAT] 구현한 soundClassifier 호출

--- MusicInEyes/MainViewController.swift
+++ MusicInEyes/MainViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import SoundAnalysis
 
 class MainViewController: UIViewController {
     
@@ -27,7 +29,9 @@ class MainViewController: UIViewController {
         button.contentHorizontalAlignment = .fill
         
         button.translatesAutoresizingMaskIntoConstraints = false
-        
+        button.addTarget(self,
+                         action: #selector(startClassifyMusic),
+                         for: .touchUpInside)
         return button
     }()
     
@@ -39,7 +43,6 @@ class MainViewController: UIViewController {
         return view
     }()
     
-    /*
     private var contentLabel: UILabel = {
         let label = UILabel()
         label.text = "탭하여 ME하기"
@@ -49,7 +52,6 @@ class MainViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    */
     
     private var musicMoodImageView: UIImageView = {
         let imageView = UIImageView()
@@ -64,22 +66,29 @@ class MainViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.tintColor = .customBlack
 
+        resultsObserver.delegate = self
+        inputFormat = audioEngine.inputNode.inputFormat(forBus: 0)
+        analyzer = SNAudioStreamAnalyzer(format: inputFormat)
+        
         view.backgroundColor = .white
-        [bottomRectangle, musicMoodImageView].forEach { view.addSubview($0) }
+        [bottomRectangle, contentLabel, musicMoodImageView].forEach { view.addSubview($0) }
         bottomRectangle.addSubview(recordButton)
         configureConstraints()
         
     }
     
+    @objc func startClassifyMusic() {
+        startAudioEngine()
+    }
+    
     private func configureConstraints() {
         NSLayoutConstraint.activate([
             
-            /*
+
             contentLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             contentLabel.widthAnchor.constraint(equalTo: view.widthAnchor),
             contentLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             contentLabel.bottomAnchor.constraint(equalTo: recordButton.topAnchor, constant: -UIScreen.main.bounds.height * 0.2),
-            */
             
             musicMoodImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             musicMoodImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -96,5 +105,15 @@ class MainViewController: UIViewController {
             bottomRectangle.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width),
             bottomRectangle.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.18)
         ])
+    }
+}
+
+
+extension MainViewController: MusicMoodClassifierDelegate {
+    func displayPredictionResult(identifier: String, confidence: Double) {
+        // 여기서 결과값 반환해서 이미지와 맞는 값 찾아주기
+        DispatchQueue.main.async {
+            self.contentLabel.text = "Recognition: \(identifier) Confidence \(confidence)"
+        }
     }
 }
